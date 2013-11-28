02dee59adb69d9e737f4180ebfc4e50334f0987a
Andrew <shixish@gmail.com>
2013-11-27 18:39:32 -1000

    simplified version

--- scripts/main.js
+++ scripts/main.js
@@ -16,72 +16,20 @@ getUserMedia(function (err, stream) {
 //I'm using this as a good resource: view-source:http://stemkoski.github.io/Three.js/Video.html
 //also: http://www.html5rocks.com/en/tutorials/getusermedia/intro/
 
-var camera, scene, renderer;
-var mesh, videoTexture, videoImageContext;
-
-//preload image asset:
-var beach = THREE.ImageUtils.loadTexture( 'imgs/beach.jpg' )
+var videoImageContext;
 
 function init() {
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(dim[0], dim[1]);
-  renderer.setClearColor(0xffffff, 1);
-  //renderer.setClearColorHex( 0xffffff, 1 );
-  document.body.appendChild( renderer.domElement );
-
-  camera = new THREE.PerspectiveCamera( 100, dim[0] / dim[1], 1, 1000 );
-  camera.position.z = 200;
-
-  scene = new THREE.Scene();  
-  
   videoImage = document.createElement( 'canvas' );
 	videoImage.width = dim[0];
 	videoImage.height = dim[1];
 	document.body.appendChild( videoImage );
-
 	videoImageContext = videoImage.getContext( '2d' );
-	// background color if no video present
-	//videoImageContext.fillStyle = '#ffffff';
-	//videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );
-
-	videoTexture = new THREE.Texture( videoImage );
-	videoTexture.minFilter = THREE.LinearFilter;
-	videoTexture.magFilter = THREE.LinearFilter;
-  
-//  //It works but spams an error: WebGL: INVALID_OPERATION: generateMipmap: level 0 not power of 2 or not all the same size 
-//  videoTexture = new THREE.Texture( video );
-//	videoTexture.minFilter = THREE.LinearFilter;
-//	videoTexture.magFilter = THREE.LinearFilter;
-
-  var backgroundTexture = beach;
-  var backgroundMaterial = new THREE.MeshBasicMaterial( { map: backgroundTexture, transparent: true } );
-	var backgroundGeometry = new THREE.PlaneGeometry( dim[0], dim[1], 1, 1 );
-	var backgroundScreen = new THREE.Mesh( backgroundGeometry, backgroundMaterial );
-  backgroundScreen.position.set(0,0,0);
-  scene.add(backgroundScreen);
-  
-  var movieMaterial = new THREE.MeshBasicMaterial( { map: videoTexture, transparent: true, overdraw: true } );
-	var movieGeometry = new THREE.PlaneGeometry( dim[0], dim[1], 1, 1 );
-	var movieScreen = new THREE.Mesh( movieGeometry, movieMaterial );
-	movieScreen.position.set(0,0,0);
-	scene.add(movieScreen);
-  
-  //window.addEventListener( 'resize', onWindowResize, false );
   animate();
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize( window.innerWidth, window.innerHeight );
-
 }
 
 function animate(){
   requestAnimationFrame( animate );
-	render();		
-	//update();
+	render();
 }
 
 
@@ -133,11 +81,7 @@ function fancy() {
 
 function render(){	
 	if (video.readyState === video.HAVE_ENOUGH_DATA ){
-		//videoImageContext.drawImage( video, 0, 0 );
     chromaKey();
     //fancy();
-		if (videoTexture) videoTexture.needsUpdate = true;
 	}
-  
-	renderer.render( scene, camera );
 }
